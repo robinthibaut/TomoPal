91d87172481f32916792b3bec7e45d49a02f9910
Robin <robin.thibaut@ugent.be>
2020-06-10 12:56:09 +0200

    Minor changes

--- tomopal/scripts/conversion.py
+++ tomopal/scripts/conversion.py
@@ -2,12 +2,14 @@
 
 import os
 
+import numpy as np
+
 from tomopal.geoview.iotomo import Transformation
 
 if __name__ == '__main__':
     cwd = os.path.dirname(os.getcwd())
     file = '28'
-    data_ = os.path.join(cwd, 'data', file, f'p{file}.dat')
+    data_ = os.path.join(cwd, 'data', file, f'{file}.dat')
     dem_ = os.path.join(cwd, 'data', 'n11_e108_1arc_v3.tif')
 
     # Arbitrary geographic origin (WGS84)
@@ -17,7 +19,10 @@ if __name__ == '__main__':
     dem_bbox = ((11.147984, 108.422479), (11.284767, 108.602519))
 
     # Geographic tomography bounds (start point, end point)
-    tomo_bounds = ((11.21201674, 108.5593435), (11.21594119, 108.5563131))
+    with open(os.path.join(cwd, 'data', 'mycoordinates.txt'), 'r') as cf:
+        pc = np.array([line.split() for line in cf.readlines()])
+
+    tomo_bounds = np.flip(np.array(pc[np.where(f'P{file}' in pc[:, 0])][0][1:].astype(np.float)).reshape(-1, 2), axis=1)
 
     # Initiate transformation object
     tio = Transformation(data_, bounds=tomo_bounds, origin=origin, dem=dem_)
