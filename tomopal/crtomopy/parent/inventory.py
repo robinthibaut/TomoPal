a83c9808ec0613222ee0503bc65d74655a6ca170
Robin Thibaut <robin.thibaut@ugent.be>
2020-07-08 12:30:01 +0200

    Defined base class for directories

--- tomopal/crtomopy/parent/inventory.py
+++ tomopal/crtomopy/parent/inventory.py
@@ -5,4 +5,4 @@ from dataclasses import dataclass
 @dataclass
 class Directories:
     """Define main directories"""
-    main_dir = os.path.dirname(os.path.abspath(__file__))
+    main_dir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
