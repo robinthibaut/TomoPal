6a77891be2e21134ffe70ae181df0e6663f9bfd9
Robin <robin.thibaut@ugent.be>
2020-12-13 23:59:16 +0100

    Minor changes

---
+++ setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+
+my_pckg = find_packages(include=['tomopal'])
+print(my_pckg)
+
+setup(
+    name='tomopal',
+    version='1.0.0',
+    packages=my_pckg,
+    include_package_data=True,
+    url='https://github.com/robinthibaut/tomopal',
+    license='MIT',
+    author='Robin Thibaut',
+    author_email='robin.thibaut@UGent.be',
+    description='Your electrical resistivity tomography companion !',
+    install_requires=['numpy', 'rasterio', 'vtk', 'geographiclib', 'scipy', 'pandas', 'matplotlib'],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
